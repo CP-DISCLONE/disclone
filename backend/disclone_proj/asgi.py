f619d224ed5780160b10b163b1f96fa32c8b83a9
nathan pennington <nathantp@gmail.com>
2024-04-23 16:38:05 -0500

    docker error troubleshooting

--- backend/disclone_proj/asgi.py
+++ backend/disclone_proj/asgi.py
@@ -12,6 +12,9 @@ import django
 from channels.routing import get_default_application
 
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disclone_proj.settings')
 django.setup()
+
 application = get_default_application()
+
