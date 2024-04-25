a77fb496c28e7579a9ec41137e8ac7becc69d396
nathan pennington <nathantp@gmail.com>
2024-04-25 16:03:50 -0500

    colors and cleanup

--- frontend/src/pages/ChatRoom.tsx
+++ frontend/src/pages/ChatRoom.tsx
@@ -57,31 +57,20 @@ const ChatRoom: React.FC = () => {
                 <div className="justify-center">
                     <h1 className="text-center text-4xl">Room Name</h1>
                     <div className="grid grid-cols-8 gap-1  h-[800px]">
-                        <div className="col-span-1 bg-gray-200 p-2 m-1 flex flex-col">Channels</div>
-                        <div className="col-span-2 bg-gray-300 p-2 m-1 flex flex-col">Logged In Users:
-                            <div className="overflow-y-auto bg-slate-100 p-2 m-1 flex-wrap flex items-center ">
-                                <div className="bg-slate-200 p-2 m-1  h-[40px] w-[40px]"></div>
-                                <p className="text-slate-700">BananaSplitz</p>
+                        <div className="col-span-1 bg-royalblue-800 p-2 m-1 flex flex-col rounded-md text-lg text-gray-100">Channels:</div>
+                        <div className="col-span-2 p-2 m-1 gap-4 flex flex-col text-lg text-gray-400">Users:
+                            <div className="overflow-y-auto hover:bg-royalblue-300 p-2 m-1 flex-wrap flex items-center gap-2  border-b-2 ">
+                                <div className="bg-slate-200 p-2 m-1  h-[40px] w-[40px] border rounded-full "></div>
+                                <p className="text-slate-700 hidden lg:block">BananaSplitz</p>
                             </div>
-                            <div className=" overflow-y-auto bg-slate-100 p-2 m-1 flex-wrap flex items-center ">
-                                <div className="bg-slate-200 p-2 m-1  h-[40px] w-[40px]"></div>
-                                <p className="text-slate-700">FluffyCactus</p>
-                            </div>
-                            <div className=" overflow-y-auto bg-slate-100 p-2 m-1 flex-wrap flex items-center ">
-                                <div className="bg-slate-200 p-2 m-1  h-[40px] w-[40px]"></div>
-                                <p className="text-slate-700">JellyBeanJamboree</p>
-                            </div>
-                            <div className=" overflow-y-auto bg-slate-100 p-2 m-1 flex-wrap flex items-center ">
-                                <div className="bg-slate-200 p-2 m-1  h-[40px] w-[40px]"></div>
-                                <p className="text-slate-700">GalacticPancake</p>
-                            </div>
+                           
 
 
 
                         </div>
-                        <div className="col-span-5 bg-gray-400 p-2 m-1 flex flex-col justify-between">
+                        <div className="col-span-5   m-2  flex flex-col justify-between">
                             {/* Chat messages */}
-                            <div className="h-[720px] overflow-y-auto bg-slate-100 p-2  flex-col justify-end">
+                            <div className="h-[720px] overflow-y-auto p-4 rounded-md bg-slate-100 flex-col justify-end">
                                 
                                 
                               
@@ -106,8 +95,8 @@ const ChatRoom: React.FC = () => {
 
                             
                             
-                            <form onSubmit={(e) => { handleSend(e) }} className="mt-4 container flex gap-2">
-                                <button className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2">Send</button>
+                            <form onSubmit={(e) => { handleSend(e) }} className="container flex gap-2 p-3">
+                                <button className="bg-royalblue-800 text-white px-3 rounded-md ml-1">Send</button>
                                 <input
                                     type="text"
                                     value={inputMsg}
