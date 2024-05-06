9d1c214b8bc8ff2d4d15bf5bc25ba2a28c2b69f4
Tom Childress <thomas.childress02@gmail.com>
2024-05-05 22:07:23 -0400

    update: configured application to serve nginx for production, ec2 testing incoming

--- backend/disclone_proj/settings.py
+++ backend/disclone_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1p!ab&we1ibu4bf)8tnuxgn)vptp7e!jb*mchjkspbfb(14)t&'
+SECRET_KEY = env.get("DJANGO_SECRET_KEY", "top-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -49,10 +49,12 @@ INSTALLED_APPS = [
     'server_app'
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:5174"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:5174"
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
