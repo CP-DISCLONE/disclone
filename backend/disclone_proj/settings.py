6d87339e9dc5dd3ec7bd66fbf643b116f22d897b
Tom Childress <thomas.childress02@gmail.com>
2024-05-07 17:47:31 -0400

    fixed allowed hosts

--- backend/disclone_proj/settings.py
+++ backend/disclone_proj/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = env.get("DJANGO_SECRET_KEY", "top-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = [
+    "http://gunicorn",
+    "http://daphne"
+]
 
 
 # Application definition
