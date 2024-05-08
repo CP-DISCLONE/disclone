7b4841d1dd969cff3bd8fa95bfa336aea049b347
Tom Childress <thomas.childress02@gmail.com>
2024-05-08 00:32:31 -0400

    updated https traffic for api instance

--- frontend/src/utilities/axiosInstance.tsx
+++ frontend/src/utilities/axiosInstance.tsx
@@ -4,5 +4,5 @@ import axios, { AxiosInstance } from "axios";
  * @description An AxiosInstance configured with the base server url for requests
  */
 export const api: AxiosInstance = axios.create({
-  baseURL: "http://disclone.duckdns.org/api/v1/",
+  baseURL: "https://disclone.duckdns.org/api/v1/",
 });
