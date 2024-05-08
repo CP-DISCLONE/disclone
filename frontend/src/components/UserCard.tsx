2741c2a4b8eb23173cf4e5303bf50933e9660ae7
Tom Childress <thomas.childress02@gmail.com>
2024-05-08 14:51:45 -0400

    update: updated url path for static media files in frontend and updated title and link information for tab picture and name in browser

--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -10,7 +10,7 @@ const UserCard: React.FC<UserCardProps> = ({ user }: UserCardProps): ReactElemen
     return (
         <>
             <div className="overflow-y-auto hover:bg-royalblue-300 p-2 m-1 flex-wrap flex items-center gap-2  rounded-md bg-background ">
-                {user.profilePicture ? <img className='rounded' alt={`${user.displayName}'s Profile Picture`} src={`http://localhost:8001${user.profilePicture}`} /> : <img className='rounded' src={defaultProfilePicture} alt={`${user.displayName}'s Profile Picture`} />}
+                {user.profilePicture ? <img className='rounded' alt={`${user.displayName}'s Profile Picture`} src={`https://disclone.duckdns.org${user.profilePicture}`} /> : <img className='rounded' src={defaultProfilePicture} alt={`${user.displayName}'s Profile Picture`} />}
                 {user ? <p className="text-foreground hidden lg:block">{user.displayName}</p> : null}
             </div>
         </>
